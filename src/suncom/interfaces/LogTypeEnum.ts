a7d148ac7c999e5039b1eed6cadade8e474b5bb2
syfolen <syfolen@163.com>
2020-03-16 11:39:39 +0800

    LOG增加类型

--- src/suncom/interfaces/LogTypeEnum.ts
+++ src/suncom/interfaces/LogTypeEnum.ts
@@ -7,21 +7,25 @@ module suncom {
     export enum LogTypeEnum {
         /**
          * 普通日志
+         * export
          */
         VERBOSE,
 
         /**
          * 警告日志
+         * export
          */
         WARN,
 
         /**
          * 错误日志
+         * export
          */
         ERROR,
 
         /**
          * 文件日志
+         * export
          */
         LOG2F
     }
